d4bb419b1af6d1a960c811678f188676cd1ca890
Peiris M. H. C. <154981814+IT22194930@users.noreply.github.com>
2025-04-23 09:17:28 +0530

    style: Update button text from 'Submit Registration' to 'Save Registration' in OrganizationForm component and create_tables.sql file

--- client/src/components/OrganizationForm.jsx
+++ client/src/components/OrganizationForm.jsx
@@ -537,7 +537,7 @@ const OrganizationForm = () => {
               </div>
             ) : (
               <div className="flex items-center justify-center">
-                <span>Submit Registration</span>
+                <span>Save Registration</span>
                 <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 ml-2" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                   <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M14 5l7 7m0 0l-7 7m7-7H3" />
                 </svg>
